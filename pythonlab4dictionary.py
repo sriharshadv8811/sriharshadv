4ec567dd15145867c162fb38c69dce79b07cf2c9
sriharshadv8811 <35754248+sriharshadv8811@users.noreply.github.com>
2018-02-28 09:58:21 +0530

    Add files via upload

---
+++ pythonlab4dictionary.py
@@ -0,0 +1,7 @@
+d={1:"hi","py":30}
+print(d[1])
+print(d['py'])
+d={1:"welcome",2:"to",3:"python"}
+print(d[1],d[2],d[3])
+d={1:"my",2:"name",3:"is",4:"sri",5:"harsha"}
+print(d[1],d[2],d[3],d[4],d[5])
